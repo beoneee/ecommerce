5311fe4dd7142a88e0478f4a00c17907f9b3ad58
Manh Hung <2210hung@gmail.com>
2024-11-14 23:09:54 +0700

    updated

---
+++ app/(frontend)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex items-center justify-center flex-col min-h-screen'>
+      <h2 className='text-4xl font-bold'>Welcome to Limi Ecommerce</h2>
+    </div>
+  )
+}
